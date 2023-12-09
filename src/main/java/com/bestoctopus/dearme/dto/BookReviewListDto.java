5a3745fb2e240de149d9eb3c84247b3105476454
chamny20 <chamny20@naver.com>
2023-12-09 14:25:19 +0900

    feat : add BookReviewListDto (#3)

---
+++ src/main/java/com/bestoctopus/dearme/dto/BookReviewListDto.java
@@ -0,0 +1,26 @@
+package com.bestoctopus.dearme.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BookReviewListDto {
+
+    private String reviewTitle;
+
+    private String contents;
+
+    private String lastEdit;
+
+    private String title;
+
+    private String img;
+
+    private String nickname;
+
+    
+}
