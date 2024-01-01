2e56191dd34b93dd271952244a9c50e72b24780b
정문영 <jmuny@jeongmun-yeong-ui-MacBookAir.local>
2024-01-01 21:40:37 +0900

    fix : fixed bucketlist api (get/post/put/delete) (#8)

--- src/main/java/com/bestoctopus/dearme/controller/BucketListController.java
+++ src/main/java/com/bestoctopus/dearme/controller/BucketListController.java
@@ -1,9 +1,11 @@
 package com.bestoctopus.dearme.controller;
 
 import com.bestoctopus.dearme.domain.BucketList;
+import com.bestoctopus.dearme.domain.User;
 import com.bestoctopus.dearme.dto.BucketListDto;
 import com.bestoctopus.dearme.dto.PutBucketListDto;
 import com.bestoctopus.dearme.dto.ResultDto;
+import com.bestoctopus.dearme.repository.UserRepository;
 import com.bestoctopus.dearme.service.BucketListService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,11 +14,15 @@ import org.springframework.cglib.core.Local;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.transform.Result;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/bucket")
@@ -25,31 +31,41 @@ public class BucketListController {
 
     private final BucketListService bucketListService;
 
+    private final UserRepository userRepository;
 
     @GetMapping("")
-    public ResponseEntity<?> getBucketList(@RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+    public List<BucketListDto> getBucketList(@RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
                                              @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
 
-        return ResponseEntity.ok(bucketListService.getAllBucketList(startDate, endDate));
+        List<BucketList> bucketLists = bucketListService.getAllBucketList(startDate, endDate);
 
+        List<BucketListDto> collect = bucketLists.stream()
+                .map(m-> new BucketListDto(m.getStatus(), m.getGoal(), m.getDate()))
+                .collect(Collectors.toList());
+
+        return collect;
     }
+
 
     @PostMapping("")
     public ResponseEntity<?> PostBucketList( // @RequestHeader("Authorization") String accessToken,
                                              @RequestBody @Valid BucketListDto bucketListDto) {
 
-        bucketListService.postBucketList(bucketListDto.toEntity());
+        String user_id = "1";
+
+        bucketListService.postBucketList(bucketListDto, user_id);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
-    @PutMapping("/{itemId}")
+    @PutMapping("/{bucket_id}")
     public ResponseEntity<?> putBucketList(//@RequestHeader("Authorization") String accessToken,
-                                            @PathVariable("bucket_id") Long bucketId,
-                                            @RequestBody @Valid PutBucketListDto putBucketListDto) {
+                                          @PathVariable("bucket_id") Long bucketId) {
 
-        boolean updated = bucketListService.putBucketList(bucketId, putBucketListDto);
+        String user_id = "1";
+
+        boolean updated = bucketListService.putBucketList(bucketId, user_id);
 
         if (updated) {
             return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/com/bestoctopus/dearme/service/BucketListService.java
+++ src/main/java/com/bestoctopus/dearme/service/BucketListService.java
@@ -1,15 +1,20 @@
 package com.bestoctopus.dearme.service;
 
 import com.bestoctopus.dearme.domain.BucketList;
+import com.bestoctopus.dearme.domain.User;
+import com.bestoctopus.dearme.dto.BucketListDto;
 import com.bestoctopus.dearme.dto.PutBucketListDto;
 import com.bestoctopus.dearme.repository.BucketListRepository;
+import com.bestoctopus.dearme.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -18,22 +23,35 @@ public class BucketListService {
 
     private final BucketListRepository bucketListRepository;
 
+    private final UserRepository userRepository;
+
     public List<BucketList> getAllBucketList(LocalDate startDate, LocalDate endDate) {
         return bucketListRepository.findBydateBetween(startDate, endDate);
     }
 
-    public BucketList postBucketList(BucketList bucketList) {
+
+    public BucketList postBucketList(BucketListDto bucketListDto, String user_id) {
+        User user = userRepository.findById(user_id).orElseThrow();
+
+        BucketList bucketList = bucketListDto.toEntity(user);
+
         return bucketListRepository.save(bucketList);
     }
 
-    public boolean putBucketList(Long bucketId, PutBucketListDto putBucketListDto) {
-        Optional<BucketList> optionalBucketList = bucketListRepository.findById(bucketId);
+    public boolean putBucketList(Long bucket_id, String user_id) {
+        User user = userRepository.findById(user_id).orElseThrow();
+
+        Optional<BucketList> optionalBucketList = bucketListRepository.findById(bucket_id);
 
         if (optionalBucketList.isPresent()) { //bucketId가 존재할 경우에만 update 실행
             BucketList bucketList = optionalBucketList.get();
-            bucketList.update(putBucketListDto);
-            bucketListRepository.save(bucketList);
-            return true;
+            if (bucketList.getUser().equals(user)) {
+                Boolean status = bucketList.getStatus();
+                bucketList.update(!status);
+                bucketListRepository.save(bucketList);
+                return true;
+            }
+            else { return false; }
         } else {
             return false;
         }
