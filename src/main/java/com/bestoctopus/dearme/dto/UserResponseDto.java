c112861f6404d87a95d4a0e33b40a617426833d0
hyunzzii <yhynym@gmail.com>
2024-02-06 14:08:12 +0900

    fix: 로그인시 비밀번호 노출되는 오류 수정

---
+++ src/main/java/com/bestoctopus/dearme/dto/UserResponseDto.java
@@ -0,0 +1,43 @@
+package com.bestoctopus.dearme.dto;
+
+import com.bestoctopus.dearme.domain.User;
+import com.bestoctopus.dearme.exception.NotValidateException;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class UserRequestDto {
+    private String id;
+    private String password;
+    private String nickname;
+    private String name;
+
+    public User toEntity() {
+        if (this.id == null || this.password == null || this.nickname == null || this.name == null) {
+            throw new NotValidateException("유효하지 않은 입력입니다.");
+        }
+
+        return User.builder()
+                .id(this.id)
+                .password(this.password)
+                .nickname(this.nickname)
+                .name(this.name)
+                .build();
+
+    }
+
+    public static UserRequestDto fromEntity(User user) {
+        return UserRequestDto.builder()
+                .id(user.getId())
+                .password(user.getPassword())
+                .nickname(user.getNickname())
+                .name(user.getName())
+                .build();
+
+    }
+}
